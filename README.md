273022acd9b80b81ec786e758fc5341476bb0d66
Lyall <me@lyall.lol>
2025-01-02 05:42:42 +0000

    changes

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Bluemoji Downloader
-## Scrapes and downloads EVERY color/variant of the blue emojis for [bluemoji.io](https://bluemoji.io/) in less than 30 seconds!
+Scrapes and downloads every color/variant of the blue emojis from [bluemoji.io](https://bluemoji.io/)
+
+## Requirements
+A JavaScript runtime like [Node.js](https://nodejs.org/) or [Bun](https://bun.sh/)
+
+## Usage
+Just run `index.js` with your runtime, eg. `node index.js`
--- index.js
+++ index.js
@@ -2,73 +2,80 @@ const fs = require("fs");
 const https = require("https");
 const path = require("path");
 
+const downloadAllAtOnce = true;
+
 // Regex
-const htmlRegex = /<a href="\/emoji\/.*?" class="img-wrapper-link is-.*? w-inline-block">\s*<img loading="lazy" alt=".*?" src=".*?"/g;
-const emojiRegex = /<a href="\/emoji\/(.*?)" class="img-wrapper-link is-(.*?) w-inline-block">\s*<img loading="lazy" alt="(.*?)" src="(.*?)"/;
-const fileRegex = /\\|\/|\*|\?|\"|\<|\>|\|/g;
-const htmlHexRegex = /&#x([a-fA-F0-9]+);/g
+const emojisRegex = /<a href="\/emoji\/.+?" class="img-wrapper-link is-.+? w-inline-block">\s*?<img loading="lazy" alt=".*?" src=".+?"/g;
+const emojiRegex = /<a href="\/emoji\/(.+?)" class="img-wrapper-link is-(.+?) w-inline-block">\s*?<img loading="lazy" alt="(.*?)" src="(.+?)"/;
 
-const names = {};
-const dirs = {};
+const emojisPath = path.resolve("Emojis");
 
-(async () => {
-    if (!fs.existsSync("Emojis")) fs.mkdirSync("Emojis"); // Create Emojis directory
+(async function main() {
+    if (!fs.existsSync(emojisPath)) fs.mkdirSync(emojisPath); // Create Emojis directory
 
     const html = await getPage().catch(() => console.log("Failed to get page!")); // Get https://bluemoji.io/
-    const htmlMatch = html.match(htmlRegex); // Match all emoji elements
+    const htmlEmojis = html.match(emojisRegex); // Match all emoji HTML elements
 
     const startDate = Date.now();
 
-    // Download all emojis
-    await (async function downloadEmoji(index = 0) {
-        const [match, name, color, alt, imgPath] = htmlMatch[index].match(emojiRegex); // Try to get emoji name, color and image URL
-        if (!name || !color || !imgPath) {
-            // Skip emoji if name, color or image URL not matched
-            console.log(`Couldn't get full match of an emoji! Match: ${match}`);
-            return downloadNextEmoji();
-        }
+    let downloaded = 0;
+    let failed = 0;
 
-        const imgUrl = imgPath.startsWith("/") ? `https://bluemoji.io${imgPath}` : imgPath;
+    // Loop through each emoji HTML element
+    for (const htmlEmoji of htmlEmojis) {
+        const [match, name, color, realName, imgPath] = htmlEmoji.match(emojiRegex); // Try to get emoji details
+        const imgUrl = imgPath.replace(/^\/+/, "https://bluemoji.io/");
 
-        if (alt) names[name] = fixName(alt);
-        const fixedName = names[name] || fixName(name);
+        const fixedName = fixName(realName || name);
 
-        // Create directory for emoji color if not already created
-        const emojiColorPath = path.join("Emojis", fixFilePath(upperCase(color)));
-        if (!dirs[color]) {
-            if (!fs.existsSync(emojiColorPath)) fs.mkdirSync(emojiColorPath);
-            dirs[color] = true;
-        }
+        const emojiColorPath = path.join(emojisPath, capitalize(color));
+        if (!fs.existsSync(emojiColorPath)) fs.mkdirSync(emojiColorPath);
 
         const emojiPath = path.join(emojiColorPath, fixFilePath(`${fixedName}${path.extname(imgUrl) || ".png"}`));
 
         if (fs.existsSync(emojiPath)) {
-            // Skip emoji if already downloaded
             log(`Already downloaded '${fixedName}' in ${color}`, color);
-        } else {
-            // Download emoji if not already downloaded
-            await downloadImg(imgUrl, emojiPath);
-            log(`Downloaded '${fixedName}' in ${color}`, color);
+            downloaded++;
+            checkProgress();
+            continue;
         }
 
-        // Download next emoji
-        return downloadNextEmoji();
+        if (downloadAllAtOnce) {
+            download();
+        } else {
+            await download();
+        }
 
-        function downloadNextEmoji() { if (htmlMatch[index + 1]) return downloadEmoji(index + 1) };
-    })();
+        async function download() {
+            try {
+                await downloadImg(imgUrl, emojiPath);
+                log(`Downloaded '${fixedName}' in ${color}`, color);
+                downloaded++;
+            } catch (err) {
+                log(`Failed to download '${fixedName}' in ${color}: ${err}`, "red");
+                failed++;
+            }
+            checkProgress();
+        }
+    }
 
-    // Done
-    const endDate = Date.now();
-    console.log(`Done! Took ${(endDate - startDate) / 1000} seconds.`);
+    function checkProgress() {
+        if ((downloaded + failed) >= htmlEmojis.length) {
+            const endDate = Date.now();
+            console.log(`Downloaded ${downloaded} of ${htmlEmojis.length} emojis in ${(endDate - startDate) / 1000} seconds`);
+        }
+    }
 })();
+
+// Functions
 
 function getPage() {
     return new Promise((resolve, reject) => {
         https.get("https://bluemoji.io/", res => {
-            if (res.statusCode != 200 || res.headers["content-type"] != "text/html") return reject(res);
-            let data = "";
-            res.on("data", i => data += i);
-            res.on("end", () => resolve(data));
+            if (res.statusCode !== 200 || res.headers["content-type"] !== "text/html") return reject(`Got status code ${res.statusCode} and content type ${res.headers["content-type"]}`);
+            const data = [];
+            res.on("data", i => data.push(i));
+            res.on("end", () => resolve(Buffer.concat(data).toString()));
         });
     });
 }
@@ -76,48 +83,49 @@ function getPage() {
 function downloadImg(url, savePath) {
     return new Promise((resolve, reject) => {
         https.get(url, res => {
-            if (res.statusCode != 200) return reject(res);
-            let data;
-            res.on("data", i => data = data ? Buffer.concat([data, i]) : i);
+            if (res.statusCode !== 200) return reject(`Got status code ${res.statusCode}`);
+            const data = [];
+            res.on("data", i => data.push(i));
             res.on("end", () => {
-                fs.writeFileSync(savePath, data);
-                resolve(res);
+                fs.writeFileSync(savePath, Buffer.concat(data));
+                resolve();
             });
         });
     });
 }
 
 function fixName(name) {
-    return decodeHtmlHex(name).split("-").map(upperCase).join(" ");
+    return decodeHtmlHex(name).split("-").map(capitalize).join(" ");
 }
 
 function decodeHtmlHex(string) {
-    return string.replace(htmlHexRegex, (match, hex) => String.fromCharCode(parseInt(hex, 16)));
+    return string.replace(/&#x([a-fA-F0-9]+);/g, (match, hex) => String.fromCharCode(parseInt(hex, 16)));
 }
 
 function fixFilePath(path) {
-    return path.replace(fileRegex, "");
+    return path.replace(/\\|\/|\*|\?|\"|\<|\>|\|/g, "");
 }
 
-function upperCase(string) { return `${string.charAt(0).toUpperCase()}${string.substring(1)}` }
+function capitalize(string) { return `${string.charAt(0).toUpperCase()}${string.substring(1)}` }
 
 function log(string, color) {
-    console.log(`${log.colors[color?.toLowerCase()] || ""}${string}${log.reset}`);
+    const colors = {
+        // https://stackoverflow.com/questions/9781218/how-to-change-node-jss-console-font-color
+        // https://en.wikipedia.org/wiki/ANSI_escape_code#Colors
+        black: "\x1b[90m", // Changed to gray
+        red: "\x1b[31m",
+        green: "\x1b[32m",
+        yellow: "\x1b[33m",
+        blue: "\x1b[34m",
+        magenta: "\x1b[35m",
+        violet: "\x1b[35m", // Added
+        pink: "\x1b[95m", // Added
+        cyan: "\x1b[36m",
+        teal: "\x1b[36m", // Added
+        white: "\x1b[37m",
+        gray: "\x1b[90m"
+    };
+    const reset = "\x1b[0m";
+
+    console.log(`${colors[color?.toLowerCase()] || ""}${string}${reset}`);
 }
-log.colors = {
-    // https://stackoverflow.com/questions/9781218/how-to-change-node-jss-console-font-color
-    // https://en.wikipedia.org/wiki/ANSI_escape_code#Colors
-    black: "\x1b[90m", // Changed to gray
-    red: "\x1b[31m",
-    green: "\x1b[32m",
-    yellow: "\x1b[33m",
-    blue: "\x1b[34m",
-    magenta: "\x1b[35m",
-    violet: "\x1b[35m", // Added
-    pink: "\x1b[95m", // Added
-    cyan: "\x1b[36m",
-    teal: "\x1b[36m", // Added
-    white: "\x1b[37m",
-    gray: "\x1b[90m"
-}
-log.reset = "\x1b[0m";
